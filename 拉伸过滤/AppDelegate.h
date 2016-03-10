d000ade508cb4700acd2a3a2b8338b77dddb509c
codeliu6572 <codeliu6572@163.com>
2016-03-10 17:13:41 +0800

    Initial Commit

---
+++ 拉伸过滤/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  拉伸过滤
+//
+//  Created by 刘浩浩 on 16/3/10.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 拉伸过滤/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  拉伸过滤
+//
+//  Created by 刘浩浩 on 16/3/10.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
